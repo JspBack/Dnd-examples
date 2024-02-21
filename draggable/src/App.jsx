01073bf5dad6491349bbae2056e78407835cbc9f
Jspback <bekciyusuf336@gmail.com>
2024-02-21 14:12:32 +0300

    initial

---
+++ draggable/src/App.jsx
@@ -0,0 +1,12 @@
+import Users from "./Users";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Bros</h1>
+      <Users />
+    </div>
+  );
+};
+
+export default App;
---
+++ draggable/src/Users.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import "./users.css";
+import { data } from "./data";
+import { closestCenter, DndContext } from "@dnd-kit/core";
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+const SortableUser = ({ user }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: user.id });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="user"
+    >
+      {user.name}
+    </div>
+  );
+};
+
+const Users = () => {
+  const [users, setUsers] = useState(data);
+  const [inputValue, setInputValue] = useState("");
+  const addUser = () => {
+    newUser = {
+      id: Date.now().toString(),
+      name: inputValue,
+    };
+    setInputValue("");
+    setUsers((users) => [...users, newUser]);
+  };
+
+  const onDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setUsers((users) => {
+      const oldIndex = users.findIndex((user) => user.id === active.id);
+      const newIndex = users.findIndex((user) => user.id === over.id);
+      return arrayMove(users, oldIndex, newIndex);
+    });
+  };
+
+  return (
+    <div className="users">
+      <div>Total: {users.length}</div>
+      <div className="users-form">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addUser}>Add user</button>
+      </div>
+      <div>
+        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+          <SortableContext items={users} strategy={verticalListSortingStrategy}>
+            {users.map((user) => (
+              <SortableUser key={user.id} user={user} />
+            ))}
+          </SortableContext>
+        </DndContext>
+      </div>
+    </div>
+  );
+};
+export default Users;
